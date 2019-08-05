c0c41ca5aa96fe043dc96d4c7263eab5f14aac19
Bogdan <pinchuk.brus@gmail.com>
2019-08-05 15:55:10 +0300

    Додано LesApp0

---
+++ LesApp0/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // введення даних
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\nВведіть символи: ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            string s = Console.ReadLine().ToLower();
+
+            // перевірка чи всі символи введені правильно
+            try
+            {
+                // прохід по символам
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!Symbol.All.Contains(s[i]))
+                    {
+                        throw new Exception("\n\tВведено недопустимі символи.");
+                    }
+                }
+
+                Console.WriteLine("\n\tВсі сиволи введені вірно.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            // repeat
+            DoExitOrRepeat();
+        }
+
+        /// <summary>
+        /// Метод виходу або повторення методу Main()
+        /// </summary>
+        static void DoExitOrRepeat()
+        {
+            Console.WriteLine("\n\nСпробувати ще раз: [т, н]");
+            Console.Write("\t");
+            var button = Console.ReadKey(true);
+
+            if ((button.KeyChar.ToString().ToLower() == "т") ||
+                (button.KeyChar.ToString().ToLower() == "n")) // можливо забули переключити розкладку клавіатури
+            {
+                Console.Clear();
+                Main();
+                // без використання рекурсії
+                //Process.Start(Assembly.GetExecutingAssembly().Location);
+                //Environment.Exit(0);
+            }
+            else
+            {
+                // закриває консоль
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ LesApp0/Symbol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    static class Symbol
+    {
+        private static string[] SymbolsArray { get; } =
+        {
+            "abcdefghijklmnopqrsyuvwxyz",            // 26, Англійський алфавіт
+            "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'",   // 33, Український алфавіт
+            "абвгдеёжзийклмнопрстуфхцчшъыьэюя"      // 21, Російський алфавіт
+        };
+
+        public static string All = new StringBuilder()
+            .Append(SymbolsArray[0])
+            .Append(SymbolsArray[1])
+            .Append(SymbolsArray[2])
+            .ToString();
+    }
+}
