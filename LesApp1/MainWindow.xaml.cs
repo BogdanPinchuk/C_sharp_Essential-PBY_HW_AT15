5b7a345ee52d6ddb6f5f6bf23b61d787ecb0c93b
Bogdan <pinchuk.brus@gmail.com>
2019-08-06 08:34:12 +0300

    Скорегована форма

--- LesApp1/MainWindow.xaml.cs
+++ LesApp1/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿#define MessageBox
+//#define StatusBar
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,15 +35,21 @@ namespace LesApp1
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Авиризація
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // сохранение даных
-            string login = tbLogin.Text;
-            string password = tbPassword.Text;
+            // збереження даних
+            string login = tbLogin.Text.ToLower();
+            string password = tbPassword.Text.ToLower();
 
-            // проверка логина
+            // перевірка авторизації
             try
             {
+                // перевірка логіна
                 for (int i = 0; i < login.Length; i++)
                 {
                     if (!dataStrLogin.Contains(login[i]))
@@ -47,48 +57,126 @@ namespace LesApp1
                         throw new NotValidLoginExeption();
                     }
                 }
-                tbExeption.Text = "Все ОК";
-            }
-            catch (NotValidLoginExeption ex)
-            {
-                tbExeption.Text = ex.Message;
-                return;
-            }
 
-            string dataStr = dataStrLogin + dataStrPassword;
-
-            // проверка логина
-            try
-            {
+                // перевірка пароля
                 for (int i = 0; i < password.Length; i++)
                 {
-                    if (!dataStr.Contains(password[i]))
+                    if (!(dataStrLogin + dataStrPassword).Contains(password[i]))
                     {
                         throw new NotValidPasswordExeption();
                     }
-
                 }
-                tbExeption.Text = "Все ОК";
+
+                // якщо успішно
+                Singal("Все ОК"); 
+
+            }
+            catch (NotValidLoginExeption ex)
+            {
+                Singal(ex.Message);
             }
             catch (NotValidPasswordExeption ex)
             {
-                tbExeption.Text = ex.Message;
-                return;
+                Singal(ex.Message);
             }
-
         }
 
+        /// <summary>
+        /// Аналіз лише логіна
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            // при натисканні Enter перевіряє правильність
+            if (e.Key == Key.Enter)
+            {
+                // збереження даних
+                string login = tbLogin.Text.ToLower();
+
+                try
+                {
+                    // перевірка логіна
+                    for (int i = 0; i < login.Length; i++)
+                    {
+                        if (!dataStrLogin.Contains(login[i]))
+                        {
+                            throw new NotValidLoginExeption();
+                        }
+                    }
+
+                    // якщо успішно
+                    Singal("З логіном все ОК");
+                }
+                catch (NotValidLoginExeption ex)
+                {
+                    Singal(ex.Message);
+                }
+            }
+        }
+
+        private void tbPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            // при натисканні Enter перевіряє правильність
+            if (e.Key == Key.Enter)
+            {
+                // збереження даних
+                string password = tbPassword.Text.ToLower();
+
+                // перевірка авторизації
+                try
+                {
+                    // перевірка пароля
+                    for (int i = 0; i < password.Length; i++)
+                    {
+                        if (!(dataStrLogin + dataStrPassword).Contains(password[i]))
+                        {
+                            throw new NotValidPasswordExeption();
+                        }
+                    }
+
+                    // якщо успішно
+                    Singal("З паролем все ОК");
+                }
+                catch (NotValidPasswordExeption ex)
+                {
+                    Singal(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сигнал, що символи відповідають обмеженням
+        /// </summary>
+        private void Singal(string s)
+        {
+            // якщо немає введених недопустимих символів
+#if StatusBar
+            tbExeption.Visibility = Visibility.Visible;
+            tbExeption.Text = s;
+#endif
+#if MessageBox
+            MessageBox.Show(s, "Registration",
+                    MessageBoxButton.OK, MessageBoxImage.Information); 
+#endif
+        }
     }
 
+    /// <summary>
+    /// Помилка введення символів в логіні
+    /// </summary>
     class NotValidLoginExeption : Exception
     {
-        public string Message { get; set; }
-            = "Недопустимые символы в логине";
+        public override string Message { get; }
+            = "Введені недопустимі символи в логіні";
     }
 
+    /// <summary>
+    /// Помилка введення символів в паролі
+    /// </summary>
     class NotValidPasswordExeption : Exception
     {
-        public string Message { get; set; }
-            = "Недопустимые символы в пароле";
+        public override string Message { get; }
+            = "Введені недопустимі символи в паролі";
     }
 }
